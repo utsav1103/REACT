15335b678e1c2552f2d1d74686f69504873a8543
utsav-baghela <utsavu858@gmail.com>
2024-10-05 17:11:01 +0530

    some jsx syntax

--- getting-started/jsx/src/App.js
+++ getting-started/jsx/src/App.js
@@ -1,7 +1,21 @@
 //
 
 const App = () => {
-    return <h1>This is my first react app</h1>
+    //basic syntax
+    const element = <h2>Hello Tissa</h2>
+
+    //embedding expressions
+    const name = 'React js developer';
+    const element2 =( <h1> Hello {name}  {2+2}</h1>
+    );
+    //jsx as an expression
+    const element3 = (
+        <div>
+            <h1>Hello, Darlin</h1>
+            <h2>It is {new Date().toLocaleTimeString()}</h2>
+        </div>
+    );
+    return element3;
 }
 
 export default App;
