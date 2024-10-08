3ab9023c7890a49dd283e7feaee21ebc9cbde430
utsav-baghela <utsavu858@gmail.com>
2024-10-08 17:10:41 +0530

    creating components (demo code)

---
+++ getting-started/components-in-react/src/App.js
@@ -0,0 +1,15 @@
+import Userprofile from "./components/UserProfile";
+
+
+const App = () => {
+    
+    
+    return (
+        <div>
+            {/*call or render the component*/}
+            <Userprofile/>
+        </div>
+    )
+};
+
+export default App;
---
+++ getting-started/components-in-react/src/components/UserProfile.js
@@ -0,0 +1,23 @@
+const Userprofile = ()=>{
+    return (
+        <div style={{border: "1px solid #ccc", padding:'20px', margin:'20px'}}>
+            <h2>User Profile</h2>
+            <p>
+                <b>Name</b>: Tissa..
+            </p>
+            <p>
+                <b>Email</b>: tissa@gamil.com
+            </p>
+            <p>
+                <b>Location</b>: Ratlam
+            </p>
+            <p>
+                <b>Hobby</b>: Pet owner, Musician
+            </p>
+            <p>
+                <b>about</b>: Tissa.. is a AI/ML engineer . She is passonate, hard working and self learner.
+            </p>
+        </div>
+    )
+}
+export default Userprofile;
