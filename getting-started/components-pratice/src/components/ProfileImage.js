b5fb9a139610605b685699cefff19a1c1bdab121
utsav-baghela <utsavu858@gmail.com>
2024-10-13 13:38:58 +0530

    practicing components

---
+++ getting-started/components-pratice/src/components/ProfileImage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfileImage = ({image}) => {
+  return (
+    <img className="ellipse-shape-icon" alt="icon" src={image} />
+  );
+};
+
+export default ProfileImage
