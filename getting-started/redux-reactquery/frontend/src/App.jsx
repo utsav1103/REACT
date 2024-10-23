80e0d43b0ea9ee59742f19822724c0142ada36c4
utsav-baghela <utsavu858@gmail.com>
2024-10-23 19:33:53 +0530

    using formik and yup

--- getting-started/redux-reactquery/frontend/src/App.jsx
+++ getting-started/redux-reactquery/frontend/src/App.jsx
@@ -1,13 +1,23 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./components/HomePage";
+import PublicNavbar from "./Templates/PublicNavbar";
+import Login from "./Templates/Login";
+import Register from "./Templates/Register";
+import UserProfile from "./Templates/UserProfile";
 
 function App() {
-  
-
   return (
     <>
-      <h1 className="text-3xl text-red-500 font-bold underline">
-      Hello world!
-    </h1>
+      <BrowserRouter>
+        {/* <Navbar /> */}
+        <PublicNavbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<UserProfile />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
